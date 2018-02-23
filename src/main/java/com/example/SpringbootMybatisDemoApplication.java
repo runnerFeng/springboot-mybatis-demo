067bece57a828885410ccbc7b620b08fa34b856a
liufeng <631819061@qq.com>
2018-02-23 10:13:41 +0800

    commit

--- src/main/java/com/example/SpringbootMybatisDemoApplication.java
+++ src/main/java/com/example/SpringbootMybatisDemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.example.mapper")
 public class SpringbootMybatisDemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMybatisDemoApplication.class, args);
 	}
